6e5faac6c6069c407722c6cc606d8ec1814a6197
DADDesign-Projects <p.bantigny@gmail.com>
2026-08-17 18:22:24 +0200

    Allow effects to modify certain parameters

--- OSCAR_BSP/Inc/HardwareAndCoDefines.h
+++ OSCAR_BSP/Inc/HardwareAndCoDefines.h
@@ -20,26 +20,10 @@
 #define GENERAL_UPDATE_MS  100     // General system update interval in milliseconds
 
 //**********************************************************************************
-// Daisy Seed Revision
-//**********************************************************************************
-enum class eHardRev_t{
-	Rev5,
-	Rev7
-};
-
-//**********************************************************************************
 // DryWet Parameter
 //**********************************************************************************
 constexpr float MIN_DRY = -45.0f;  // Minimum dry signal level cDryWet
 constexpr float MAX_DRY  = 0.0f;   // Maximum dry signal level cDryWet
 constexpr float FAD_TIME = 5.0f;   // Dry/Wet Fading time in second
 
-//**********************************************************************************
-// Audio Manager
-//**********************************************************************************
-#define AUDIO_BUFFER_SIZE  4        // Audio buffer size in samples
-#define SAMPLING_RATE      48000.0f // Audio sampling rate in Hz
 
-// Real-time refresh rate derived from audio parameters, filters, etc.
-constexpr float RT_RATE = SAMPLING_RATE / (float)AUDIO_BUFFER_SIZE;
-constexpr float RT_TIME = (float)AUDIO_BUFFER_SIZE / SAMPLING_RATE;
